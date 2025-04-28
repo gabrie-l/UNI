4fa9f171b7ddf63cc10531acb81acbb7ecbbf4b3
gabrie-l <gabrielmidar@gmail.com>
2025-04-28 10:50:38 -0400

    added otsu thresholding + brightness detection for junk patch removal

--- crop_img.py
+++ crop_img.py
@@ -3,6 +3,14 @@ import argparse
 import openslide
 import cv2
 import numpy as np
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    TextColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+)
 
 
 def read_svs_to_numpy(svs_path):
@@ -13,23 +21,54 @@ def read_svs_to_numpy(svs_path):
     return np.array(img)
 
 
-def crop_and_save(img, out_dir, patch_size=448):
-    h, w, _ = img.shape
-    print(h, w)
-    os.makedirs(out_dir, exist_ok=True)
+def is_foreground(patch, min_foreground_fraction=0.4, brightness_thresh=230):
+    gray = cv2.cvtColor(patch, cv2.COLOR_RGB2GRAY)
+    mean_brightness = gray.mean()
 
+    if mean_brightness > brightness_thresh:
+        return False
+
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    foreground_fraction = (thresh < 128).mean()
+    return foreground_fraction > min_foreground_fraction
+
+
+def crop_and_save(img, out_dir, patch_size=224):
+    h, w, _ = img.shape
+    os.makedirs(out_dir, exist_ok=True)
+    mask = np.zeros_like(img)
+
+    progress_bar = Progress(
+        f"Creating patches for image:",
+        TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+        BarColumn(),
+        MofNCompleteColumn(),
+        TextColumn("•"),
+        TimeElapsedColumn(),
+        TextColumn("•"),
+        TimeRemainingColumn(),
+    )
+    total_patches = ((h - patch_size) // patch_size + 1) * (
+        (w - patch_size) // patch_size + 1
+    )
     count = 0
-    for y in range(0, h - patch_size + 1, patch_size):
-        for x in range(0, w - patch_size + 1, patch_size):
-            patch = img[y : y + patch_size, x : x + patch_size]
-            filename = f"{x}_{y}.png"
-            path = os.path.join(out_dir, filename)
-            cv2.imwrite(path, cv2.cvtColor(patch, cv2.COLOR_RGB2BGR))
-            count += 1
+    with progress_bar:
+        task = progress_bar.add_task("Cropping", total=total_patches)
+        for y in range(0, h - patch_size + 1, patch_size):
+            for x in range(0, w - patch_size + 1, patch_size):
+                patch = img[y : y + patch_size, x : x + patch_size]
+                if is_foreground(patch):
+                    count += 1
+                    filename = f"{x}_{y}.png"
+                    path = os.path.join(out_dir, filename)
+                    cv2.imwrite(path, cv2.cvtColor(patch, cv2.COLOR_RGB2BGR))
+                    mask[y : y + patch_size, x : x + patch_size] = [255, 0, 0]
+                progress_bar.update(task, advance=1)
+    cv2.imwrite("./patch_mask.png", cv2.cvtColor(mask, cv2.COLOR_RGB2BGR))
     print(f"Saved {count} patches to {out_dir}")
 
 
-def process_svs(svs_path, output_root, patch_size=448):
+def process_svs(svs_path, output_root, patch_size=224):
     img = read_svs_to_numpy(svs_path)
     base_name = os.path.splitext(os.path.basename(svs_path))[0]
     out_dir = os.path.join(output_root, base_name)
@@ -39,7 +78,7 @@ def process_svs(svs_path, output_root, patch_size=448):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_dir", help="Directory containing .svs files")
-    parser.add_argument("--output_dir", help="Directory to save 448x448 patches")
+    parser.add_argument("--output_dir", help="Directory to save 224x224 patches")
     args = parser.parse_args()
 
     for fname in os.listdir(args.input_dir):
--- test_uni_ourdata.py
+++ test_uni_ourdata.py
@@ -3,34 +3,43 @@ import torch
 from torchvision import transforms
 import timm
 from huggingface_hub import login, hf_hub_download
-with open("../hf_access_tokens.txt","r") as f:
+
+with open("../hf_access_tokens.txt", "r") as f:
     token = f.readlines()[0].strip()
 
-login(token)  # login with your User Access Token, found at https://huggingface.co/settings/tokens
+login(
+    token
+)  # login with your User Access Token, found at https://huggingface.co/settings/tokens
 
 local_dir = "../assets/ckpts/uni2-h/"
 os.makedirs(local_dir, exist_ok=True)  # create directory if it does not exist
-hf_hub_download("MahmoodLab/UNI2-h", filename="pytorch_model.bin", local_dir=local_dir, force_download=True)
-timm_kwargs = {
-            'model_name': 'vit_giant_patch14_224',
-            'img_size': 224, 
-            'patch_size': 14, 
-            'depth': 24,
-            'num_heads': 24,
-            'init_values': 1e-5, 
-            'embed_dim': 1536,
-            'mlp_ratio': 2.66667*2,
-            'num_classes': 0, 
-            'no_embed_class': True,
-            'mlp_layer': timm.layers.SwiGLUPacked, 
-            'act_layer': torch.nn.SiLU, 
-            'reg_tokens': 8, 
-            'dynamic_img_size': True
-        }
-model = timm.create_model(
-    pretrained=False, **timm_kwargs
+hf_hub_download(
+    "MahmoodLab/UNI2-h",
+    filename="pytorch_model.bin",
+    local_dir=local_dir,
+    force_download=True,
 )
-model.load_state_dict(torch.load(os.path.join(local_dir, "pytorch_model.bin"), map_location="cpu"), strict=True)
+timm_kwargs = {
+    "model_name": "vit_giant_patch14_224",
+    "img_size": 224,
+    "patch_size": 14,
+    "depth": 24,
+    "num_heads": 24,
+    "init_values": 1e-5,
+    "embed_dim": 1536,
+    "mlp_ratio": 2.66667 * 2,
+    "num_classes": 0,
+    "no_embed_class": True,
+    "mlp_layer": timm.layers.SwiGLUPacked,
+    "act_layer": torch.nn.SiLU,
+    "reg_tokens": 8,
+    "dynamic_img_size": True,
+}
+model = timm.create_model(pretrained=False, **timm_kwargs)
+model.load_state_dict(
+    torch.load(os.path.join(local_dir, "pytorch_model.bin"), map_location="cpu"),
+    strict=True,
+)
 transform = transforms.Compose(
     [
         transforms.Resize(224),
@@ -40,8 +49,11 @@ transform = transforms.Compose(
 )
 model.eval()
 from PIL import Image
-image = Image.open("./crops/slide_example/9856_38080.png")
-image = transform(image).unsqueeze(dim=0) # Image (torch.Tensor) with shape [1, 3, 224, 224] following image resizing and normalization (ImageNet parameters)
+
+image = Image.open("./crops/slide_example/24864_21728.png")
+image = transform(image).unsqueeze(
+    dim=0
+)  # Image (torch.Tensor) with shape [1, 3, 224, 224] following image resizing and normalization (ImageNet parameters)
 with torch.inference_mode():
-    feature_emb = model(image) 
+    feature_emb = model(image)
 print(feature_emb)
